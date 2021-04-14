610a47fb6efa3df7345617243b593806af809709
juankarlos999 <juan_karlos_999@hotmail.com>
2021-04-14 18:08:35 -0500

    add function for generate strings

--- letter-combiner/app/Http/Controllers/CombinerController.php
+++ letter-combiner/app/Http/Controllers/CombinerController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class CombinerController extends Controller
 {
-    function generarCodigos($cantidad=20000, $longitud=7){ 
+    function generarCodigos($cantidad=20, $longitud=7){ 
 
         $arrPassResult=array(); 
         $arr = str_split('TJEUINGRTSDA'); // get all the characters into an array
@@ -16,21 +16,8 @@ class CombinerController extends Controller
             for($i=0;$i < 2;$i++){ 
                 shuffle($arr); // randomize the array
                 $aux = array_slice($arr, 0, $longitud); // get the first six (random) characters out
-                for ($indice = 0; $indice < $longitud; $indice++) { 
-                    /*
-                        El carácter ya va a estar en minúscula porque convertimos toda la cadena
-                        allá arriba
-                        Ahora vemos si dicho carácter está dentro del arreglo ["a", "e", "i", "o", "u"]
-                    */
-                    if (in_array($aux[$indice], ["A", "E", "I", "O", "U"])) {
-                        break;
-                    }
-                    elseif($indice == $longitud-1){
-                        $i--;
-                        break;
-                    }
-                }
-                $str = implode($aux); // smush them back into a string  
+
+                $str = implode($aux); // smush them back into a string 
             }
             if(!in_array($str, $arrPassResult)){ 
                 array_push($arrPassResult, $str); 
