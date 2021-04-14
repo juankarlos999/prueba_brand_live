f0aeb3f416873fd27b85193875c7969f099041ee
juankarlos999 <juan_karlos_999@hotmail.com>
2021-04-14 13:43:25 -0500

    add route test for list strings

--- letter-combiner/routes/web.php
+++ letter-combiner/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalculatorController;
+use App\Http\Controllers\CombinerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::post('/listado', [CalculatorController::class, 'calculatorCombiner'])->name('string.calculatorCombiner');
+Route::get('list-test', [CombinerController::class, 'generarCodigos']);
