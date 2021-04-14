047382f0b9cab869787e57141c25427319d485af
juankarlos999 <juan_karlos_999@hotmail.com>
2021-04-14 14:35:58 -0500

    add function for generate strings

--- letter-combiner/app/Http/Controllers/CombinerController.php
+++ letter-combiner/app/Http/Controllers/CombinerController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class CombinerController extends Controller
 {
     function generarCodigos($cantidad=2, $longitud=7){ 
-        $caracteres = "TJEUINGRTSDA"; 
+        /*$caracteres = "TJEUINGRTSDA"; 
          
         $arrPassResult=array(); 
         $index=0; 
@@ -21,7 +21,19 @@ class CombinerController extends Controller
                 array_push($arrPassResult,$aux); 
                 $index++; 
             } 
-        } 
-        return $aux; 
+        } */
+
+        $arrPassResult=array(); 
+        $arr = str_split('TJEUINGRTSDA'); // get all the characters into an array
+        $index=0; 
+        while($index<$cantidad){ 
+            for($i=0;$i<$longitud;$i++){ 
+                shuffle($arr); // randomize the array
+                $arr = array_slice($arr, 0, 6); // get the first six (random) characters out
+                $str = implode('', $arr); // smush them back into a string
+                array_push($arrPassResult, $str);
+            }
+        }
+        return $arrPassResult; 
     }  
 }
