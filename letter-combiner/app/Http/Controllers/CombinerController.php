4ba48b39813607789c877d8045c02601132c07ae
juankarlos999 <juan_karlos_999@hotmail.com>
2021-04-14 16:25:31 -0500

    add function for generate strings

--- letter-combiner/app/Http/Controllers/CombinerController.php
+++ letter-combiner/app/Http/Controllers/CombinerController.php
@@ -16,6 +16,20 @@ class CombinerController extends Controller
             for($i=0;$i < 2;$i++){ 
                 shuffle($arr); // randomize the array
                 $aux = array_slice($arr, 0, $longitud); // get the first six (random) characters out
+                for ($indice = 0; $indice < $longitud; $indice++) { 
+                    /*
+                        El carácter ya va a estar en minúscula porque convertimos toda la cadena
+                        allá arriba
+                        Ahora vemos si dicho carácter está dentro del arreglo ["a", "e", "i", "o", "u"]
+                    */
+                    if (in_array($aux[$indice], ["A", "E", "I", "O", "U"])) {
+                        break;
+                    }
+                    elseif($indice == $longitud-1){
+                        $i--;
+                        break;
+                    }
+                }
                 $str = implode($aux); // smush them back into a string  
             }
             if(!in_array($str, $arrPassResult)){ 
