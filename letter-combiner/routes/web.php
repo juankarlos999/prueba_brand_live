ab714e295d5adab93823a5846c52577b54976c12
juankarlos999 <juan_karlos_999@hotmail.com>
2021-04-14 21:03:22 -0500

    add paginate

--- letter-combiner/routes/web.php
+++ letter-combiner/routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalculatorController;
-use App\Http\Controllers\CombinerController;
-use Illuminate\Pagination\LengthAwarePaginator;
 
 /*
 |--------------------------------------------------------------------------
