8e1388cc4167d08fcad01f93e3d6919a93a80a69
juankarlos999 <juan_karlos_999@hotmail.com>
2021-04-14 23:05:27 -0500

    add tag style

--- letter-combiner/resources/views/layout/base.blade.php
+++ letter-combiner/resources/views/layout/base.blade.php
@@ -13,6 +13,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
+    <!-- Style -->
+    <style>
+        body {
+            background-color: #4CAEF0;
+        }
+    </style>
 </head>
 
 <body class="antialiased">
