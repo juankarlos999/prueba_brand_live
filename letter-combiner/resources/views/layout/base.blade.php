5f08b998dcb01f1a93e9a3c226219034a90fffcb
juankarlos999 <juan_karlos_999@hotmail.com>
2021-04-14 23:55:50 -0500

    add new style and class boostrap

--- letter-combiner/resources/views/layout/base.blade.php
+++ letter-combiner/resources/views/layout/base.blade.php
@@ -24,8 +24,8 @@
 <body>
     <header>
         <nav class="navbar navbar-dark" style="background-color: #1693eb">
-            <img src="img/letters.svg" width="100" height="80" />
-            <h1 class="display-5" style="color: whitesmoke">COMBINADOR DE LETRAS</h1>
+            <img src="/resources/img/letters.svg" width="100" height="80" />
+            <h1 class="display-5" style="color: whitesmoke; text-align:center;">COMBINADOR DE LETRAS</h1>
         </nav>
     </header>
     @yield('content')
